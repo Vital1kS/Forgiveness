553ae80194bcbdcc5de9a911e4ce1fabab28f752
Vital1kS <vitalya.shtengauer@mail.ru>
2025-08-23 15:34:27 +0600

    Change files to match developing mod

---
+++ src/main/java/me/vital1ks/forgiveness/ForgivenessMod.java
@@ -0,0 +1,8 @@
+package me.vital1ks.forgiveness;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(ForgivenessMod.MOD_ID)
+public class ForgivenessMod {
+    public static final String MOD_ID = "forgiveness";
+}
