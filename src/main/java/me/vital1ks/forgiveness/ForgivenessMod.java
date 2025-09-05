625089b109deca83fdafefed2d34b6ecbdf556b0
Vital1kS <vitalya.shtengauer@mail.ru>
2025-09-05 20:39:17 +0600

    Removed unused field

--- src/main/java/me/vital1ks/forgiveness/ForgivenessMod.java
+++ src/main/java/me/vital1ks/forgiveness/ForgivenessMod.java
@@ -4,8 +4,6 @@ package me.vital1ks.forgiveness;
 import net.fabricmc.api.ModInitializer;
 
 public class ForgivenessMod implements ModInitializer {
-    public static final String MOD_ID = "forgiveness";
-
     @Override
     public void onInitialize() {
 
